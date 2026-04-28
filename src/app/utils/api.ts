1690be72cf3d7359030e93ff7d1d9e828dade31a
Lofii <hisyam140905@gmail.com>
2026-04-28 14:03:22 +0700

    Update api.ts

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://api.peringkatpersi.id";
 export const PREFIX = "/make-server-5e1d66c4";
 
 export const getAuthHeaders = () => {
